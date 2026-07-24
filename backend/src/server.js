63592cdf78772ec7f165fcf82d0ea334c0bb189b
Arun Kumar <arunkumarns806@gmail.com>
2026-07-24 22:30:02 +0530

    Update production frontend CORS URL

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://real-time-chat-psi-mauve.vercel.app",
+      "https://real-time-chat-pi-rouge.vercel.app",
     ],
     credentials: true,
   })
